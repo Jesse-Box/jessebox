ef4d16a821534f1049d32af1d047743f9f48e132
Jesse Box <me@jessebox.net>
2020-04-06 17:52:14 +0200

    [Commit] Unknown
    When pulling the origin master from github I had to commit stuff?

--- jessebox/.cache/api-runner-ssr.js
+++ jessebox/.cache/api-runner-ssr.js
@@ -1,11 +1,11 @@
 var plugins = [{
-      plugin: require('/Users/jesse/Documents/GitHub/my-website/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('/Users/jesse/Documents/GitHub/my-website/jessebox/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('/Users/jesse/Documents/GitHub/my-website/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      plugin: require('/Users/jesse/Documents/GitHub/my-website/jessebox/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/gatsby-icon.png"},
     },{
-      plugin: require('/Users/jesse/Documents/GitHub/my-website/node_modules/gatsby-plugin-theme-ui/gatsby-ssr'),
+      plugin: require('/Users/jesse/Documents/GitHub/my-website/jessebox/node_modules/gatsby-plugin-theme-ui/gatsby-ssr'),
       options: {"plugins":[]},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
