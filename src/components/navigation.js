cfcaf38e2533a9442b78c4b31d4cac5ee69e745e
Jesse Box <me@jessebox.net>
2020-04-06 18:09:09 +0200

    [Fixed] import spelling mistake

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,64 +1,64 @@
 /** @jsx jsx */
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import { jsx, Flex, Container } from "theme-ui"
-import Icon from "./icon"
-import NavLink from "./navLink"
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import { jsx, Flex, Container } from 'theme-ui';
+import Icon from './icon';
+import NavLink from './navlink';
 
 Navigation.propTypes = {
-  siteTitle: PropTypes.string,
-}
+  siteTitle: PropTypes.string
+};
 
 Navigation.defaultProps = {
-  siteTitle: ``,
-}
+  siteTitle: ``
+};
 
 function Navigation({ siteTitle }) {
   return (
     <nav
       sx={{
         borderWidth: 0,
-        borderStyle: "primary",
-        borderColor: "muted",
+        borderStyle: 'primary',
+        borderColor: 'muted',
         margin: [2, 4, 4],
-        borderRadius: 2,
+        borderRadius: 2
       }}
     >
       <Container sx={{ maxWidth: 1 }}>
         <Flex
           sx={{
-            justifyContent: "spaceBetween",
-            alignItems: "center",
-            padding: 1,
+            justifyContent: 'spaceBetween',
+            alignItems: 'center',
+            padding: 1
           }}
         >
           <Link
-            to="/"
-            activeClassName="active"
+            to='/'
+            activeClassName='active'
             sx={{
-              variant: "navLink",
+              variant: 'navLink'
             }}
           >
             {siteTitle}
           </Link>
-          <Flex sx={{ alignItems: "center" }}>
+          <Flex sx={{ alignItems: 'center' }}>
             <NavLink
-              title="Twitter Profile"
-              href="https://twitter.com/JesseThomasBox"
+              title='Twitter Profile'
+              href='https://twitter.com/JesseThomasBox'
             >
-              <Icon name="twitter" />
+              <Icon name='twitter' />
             </NavLink>
             <NavLink
-              title="LinkedIn Profile"
-              href="https://www.linkedin.com/in/jesse-box-835346b7/"
+              title='LinkedIn Profile'
+              href='https://www.linkedin.com/in/jesse-box-835346b7/'
             >
-              <Icon name="linkedIn" />
+              <Icon name='linkedIn' />
             </NavLink>
           </Flex>
         </Flex>
       </Container>
     </nav>
-  )
+  );
 }
 
-export default Navigation
+export default Navigation;
