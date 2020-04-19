e23007ae6c822c5180996f09192b0640dbcf0c09
Jesse Box <me@jessebox.net>
2020-04-19 10:27:26 +0200

    [TEST] Gatsby build

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/jessebox/GitHub/my-website/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/jessebox/GitHub/my-website/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/jessebox/GitHub/my-website/src/pages/index.js"))),
   "component---src-pages-page-2-js": hot(preferDefault(require("/Users/jessebox/GitHub/my-website/src/pages/page-2.js")))
