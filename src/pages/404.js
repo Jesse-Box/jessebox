61892406a88b1e984150ae4900d8c4f88b740827
Jesse Box <me@jessebox.net>
2020-04-19 10:19:59 +0200

    [Modified] Imports

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,12 +3,14 @@ import { Styled } from "theme-ui";
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <Styled.h1>404</Styled.h1>
-    <Styled.p>Page not found</Styled.p>
-  </Layout>
-);
+function NotFoundPage() {
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <Styled.h1>404</Styled.h1>
+      <Styled.p>Page not found</Styled.p>
+    </Layout>
+  );
+}
 
 export default NotFoundPage;
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,13 +4,15 @@ import { Styled } from "theme-ui";
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <Styled.h1>Hi from the second page</Styled.h1>
-    <Styled.p>Welcome to page 2</Styled.p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-);
+function SecondPage() {
+  return (
+    <Layout>
+      <SEO title="Page two" />
+      <Styled.h1>Hi from the second page</Styled.h1>
+      <Styled.p>Welcome to page 2</Styled.p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  );
+}
 
 export default SecondPage;
