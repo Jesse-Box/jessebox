bd193727fc88a5001dce6568e1886e118a230da4
Jesse Box <me@jessebox.net>
2020-05-23 13:16:31 +0200

    [Changed] Content to be a query
    Fixes #25 #24

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,19 +1,32 @@
 import React from "react";
+import { graphql } from "gatsby";
 import { Styled } from "theme-ui";
 import Layout from "../components/Layout";
 
-function NotFoundPage() {
+function NotFoundPage({ data }) {
+  console.log(data);
   return (
     <Layout
       horizonContent={
         <>
-          <Styled.h1>404</Styled.h1>
-          <Styled.h5>Page not found</Styled.h5>
+          <Styled.h1>{data.markdownRemark.frontmatter.title}</Styled.h1>
+          <Styled.h5>{data.markdownRemark.excerpt}</Styled.h5>
         </>
       }
       pageTitle="404: Not found"
     ></Layout>
   );
 }
+
+export const query = graphql`
+  query PageNotFoundPageQuery {
+    markdownRemark(frontmatter: { title: { eq: "404" } }) {
+      frontmatter {
+        title
+      }
+      excerpt
+    }
+  }
+`;
 
 export default NotFoundPage;
