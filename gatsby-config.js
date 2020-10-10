645772b055a82cccb8334015a68afb5ae24198e0
Jesse Box <me@jessebox.net>
2020-10-10 14:57:39 +0200

    Removed the content directory and cleaned up my config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,4 @@
 module.exports = {
-  siteMetadata: {
-    title: `Jesse Box`,
-    author: {
-      name: `Jesse Box`,
-      summary: `Senior product designer at EyeEm, formally at Hubrick & Dailymotion.`,
-    },
-    description: `My personal space for expressing thoughts on design related topics`,
-    defaultThumb: ``,
-    siteUrl: `https://www.jessebox.net/`,
-    social: {
-      twitter: `@JesseThomasBox`,
-      linkedin: `jesse-box-835346b7`,
-    },
-  },
   plugins: [
     {
       resolve: `gatsby-plugin-typescript`,
@@ -32,14 +18,6 @@ module.exports = {
           emitWarning: true,
           failOnError: false,
         },
-      },
-    },
-
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
       },
     },
     {
@@ -83,18 +61,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Jesse Box`,
-        short_name: `Jesse Box`,
-        start_url: `/`,
-        background_color: `#101010`,
-        theme_color: `#F9FAFB`,
-        display: `minimal-ui`,
-        icon: `content/assets/jessebox-logomark.png`,
-      },
-    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-theme-ui`,
     `gatsby-plugin-sitemap`,
