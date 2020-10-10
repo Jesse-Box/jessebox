94c5de0eb527ea1d3078211e1e1a83d39c812932
Jesse Box <me@jessebox.net>
2020-10-10 15:26:27 +0200

    Removed some commented plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,8 +76,5 @@ module.exports = {
         disableLiveReload: false,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
