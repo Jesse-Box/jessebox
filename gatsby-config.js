9ad0da2c66031573be5db86e1118a5d19fbf47e0
Jesse Box <me@jessebox.net>
2021-03-21 17:47:15 +0100

    [Removed] declared plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,7 +61,6 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
-          `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
         ],
       },
