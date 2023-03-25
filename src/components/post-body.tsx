7fb3bbc5707507f5edbedf2a29a25806224b6dd3
Jesse Box <me@jessebox.net>
2023-03-25 12:23:02 +0100

    Update:  DatoCmsExternalEmbed parameter

--- src/components/post-body.tsx
+++ src/components/post-body.tsx
@@ -8,19 +8,25 @@ export default function PostBody({ content }) {
       <StructuredText
         data={content}
         renderBlock={({ record }) => {
-          if (record.__typename === "DatoCmsVisual") {
-            return (
-              <div className="post__visual">
-                <GatsbyImage image={record.image.gatsbyImageData} />
-              </div>
-            )
-          } else
-            return (
-              <>
-                <p>Don't know how to render a block!</p>
-                <pre>{JSON.stringify(record, null, 2)}</pre>
-              </>
-            )
+          switch (record.__typename) {
+            case "DatoCmsVisual":
+              return (
+                <div className="post__visual">
+                  <GatsbyImage image={record.image.gatsbyImageData} />
+                </div>
+              )
+            case "DatoCmsExternalEmbed":
+              return (
+                <div className="post__visual">{record.externalVideo.url}</div>
+              )
+            default:
+              return (
+                <>
+                  <p>Don't know how to render a block!</p>
+                  <pre>{JSON.stringify(record, null, 2)}</pre>
+                </>
+              )
+          }
         }}
       />
     </section>
